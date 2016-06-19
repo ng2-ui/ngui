451c7caf1bc41ff984229f513bad67d96587bb31
Allen Kim <allenhwkim@gmail.com>
2016-06-19 17:11:17 -0400

    0.1.6

--- dist/directives.js
+++ dist/directives.js
@@ -1,10 +1,10 @@
 "use strict";
-var jui_auto_complete_1 = require('./jui-auto-complete');
-exports.AutoCompleteDirective = jui_auto_complete_1.AutoCompleteDirective;
-var jui_datetime_picker_1 = require('./jui-datetime-picker');
-exports.DateTimePickerDirective = jui_datetime_picker_1.DateTimePickerDirective;
-var jui_map_1 = require('./jui-map');
-exports.NG2_MAP_DIRECTIVES = jui_map_1.NG2_MAP_DIRECTIVES;
+var index_1 = require('./jui-auto-complete/index');
+exports.AutoCompleteDirective = index_1.AutoCompleteDirective;
+var index_2 = require('./jui-datetime-picker/index');
+exports.DateTimePickerDirective = index_2.DateTimePickerDirective;
+var index_3 = require('./jui-map/index');
+exports.NG2_MAP_DIRECTIVES = index_3.NG2_MAP_DIRECTIVES;
 var jsvalley_directives_1 = require('./jsvalley_directives');
 exports.JSVALLEY_DIRECTIVES = jsvalley_directives_1.JSVALLEY_DIRECTIVES;
 //# sourceMappingURL=directives.js.map
--- dist/jsvalley_directives.js
+++ dist/jsvalley_directives.js
@@ -1,10 +1,10 @@
 "use strict";
-var jui_auto_complete_1 = require('./jui-auto-complete');
-var jui_datetime_picker_1 = require('./jui-datetime-picker');
-var jui_map_1 = require('./jui-map');
+var index_1 = require('./jui-auto-complete/index');
+var index_2 = require('./jui-datetime-picker/index');
+var index_3 = require('./jui-map/index');
 exports.JSVALLEY_DIRECTIVES = [
-    jui_auto_complete_1.AutoCompleteDirective,
-    jui_datetime_picker_1.DateTimePickerDirective,
-    jui_map_1.NG2_MAP_DIRECTIVES
+    index_1.AutoCompleteDirective,
+    index_2.DateTimePickerDirective,
+    index_3.NG2_MAP_DIRECTIVES
 ];
 //# sourceMappingURL=jsvalley_directives.js.map
