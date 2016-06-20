4cd7b682d897a4ba93aeb45516f6f41160ea070f
Allen Kim <allenhwkim@gmail.com>
2016-06-20 19:28:25 -0400

    0.1.6

--- dist/directives.d.ts
+++
@@ -1,4 +0,0 @@
-export { AutoComplete, AutoCompleteComponent, AutoCompleteDirective } from './jui-auto-complete/index';
-export { DateTime, DateTimePickerComponent, DateTimePickerDirective } from './jui-datetime-picker/index';
-export { NG2_MAP_DIRECTIVES } from './jui-map/index';
-export { JSVALLEY_DIRECTIVES } from './jsvalley_directives';
--- dist/jsvalley_directives.ts
+++
@@ -1,9 +0,0 @@
-import { AutoCompleteDirective } from './jui-auto-complete/index';
-import { DateTimePickerDirective } from './jui-datetime-picker/index';
-import { NG2_MAP_DIRECTIVES } from './jui-map/index';
-
-export const JSVALLEY_DIRECTIVES: any[] = [
-  AutoCompleteDirective,
-  DateTimePickerDirective,
-  NG2_MAP_DIRECTIVES
-];
