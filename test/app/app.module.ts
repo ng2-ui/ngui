9b56442270328e243b28de26bf48643b7da5a063
Allen Kim <allenhwkim@gmail.com>
2016-08-20 16:16:49 -0400

    rc5 upgrade

--- test/app/app.module.ts
+++ test/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from "@angular/forms";
 import { HTTP_PROVIDERS } from "@angular/http";
 
-import { AppComponent }  from './app.component';
-import {Ng2UIModule} from "../../src/ng2-ui.module";
+import { AppComponent }   from './app.component';
+import { Ng2UIModule }    from "../../src/ng2-ui.module";
 
 @NgModule({
-  imports: [ BrowserModule, Ng2UIModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports: [BrowserModule, Ng2UIModule, FormsModule],
+  declarations: [AppComponent],
   providers: [HTTP_PROVIDERS],
   bootstrap: [ AppComponent ]
 })
