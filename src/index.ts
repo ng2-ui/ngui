ccf4e5bc9b87f4a990ba993e5c6c8422681ff1ec
Allen Kim <allenhwkim@gmail.com>
2016-09-11 20:27:14 -0400

    updated package.json

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { BrowserModule  } from '@angular/platform-browser';
 
-import { OverlayManager, Ng2OverlayModule }  from 'ng2-overlay';
-
+import {
+  OverlayManager,
+  Ng2OverlayModule
+}  from 'ng2-overlay';
 import { Ng2AutoCompleteModule }   from "ng2-auto-complete";
 import { Ng2DatetimePickerModule } from "ng2-datetime-picker";
 import { Ng2CollapsableModule }    from "ng2-collapsable";
 import { Ng2InfiniteListModule }   from "ng2-infinite-list";
-import { Ng2MapModule, Ng2MapComponent }            from "ng2-map";
+import {
+  Ng2MapModule,
+  Ng2MapComponent
+} from "ng2-map";
 import { Ng2MenuModule }           from "ng2-menu";
 import { Ng2ParallaxScrollModule } from "ng2-parallax-scroll";
-import { Ng2PopupModule, Ng2MessagePopupComponent, Ng2PopupComponent }          from "ng2-popup";
+import {
+  Ng2PopupModule,
+  Ng2MessagePopupComponent,
+  Ng2PopupComponent
+}             from "ng2-popup";
 import { Ng2StickyModule }         from "ng2-sticky";
 import { Ng2TabModule }            from "ng2-tab";
 import { Ng2TooltipOverlayModule } from "ng2-tooltip-overlay";
-import { Ng2ScrollableModule, Ng2ScrollableDirective }     from "ng2-scrollable";
+import {
+  Ng2ScrollableModule,
+  Ng2ScrollableDirective
+}         from "ng2-scrollable";
 import { Ng2SortableModule }       from "ng2-ui-sortable";
 
 export {
