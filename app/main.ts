e2f0e5f7ef1ad7685676d545005a33cab16ddb22
Allen Kim <allenhwkim@gmail.com>
2016-09-17 17:58:01 -0400

    0.11.4 angular 2.0.0

--- app/main.ts
+++ app/main.ts
@@ -6,9 +6,10 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from "@angular/forms";
 import { HttpModule } from '@angular/http';
+import { Ng2UIModule }    from 'ng2-ui';
 
 import { AppComponent }   from './app.component';
-import { Ng2UIModule }    from 'ng2-ui';
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpModule, Ng2UIModule],
